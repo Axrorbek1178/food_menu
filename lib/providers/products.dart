e5e611f6931a7bda783046036c3ccd1ef0adc399
Axrorbek <axrorbekaminjonov123@gmail.com>
2025-12-09 08:27:53 +0500

    new product added

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -25,7 +25,7 @@ class Products with ChangeNotifier {
       description: 'Iphone 17 Pro',
       price: 17000,
       imageUrl:
-          'https://images.unsplash.com/photo-1603351154351-5e2d0600bb77?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+          'https://images.unsplash.com/photo-1759588073186-1d4ac7e33623?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     ),
   ];
 
