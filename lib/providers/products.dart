9e56ad77184d8e4fceb9c980670fb05b503f9068
Axrorbek <axrorbekaminjonov123@gmail.com>
2025-12-10 19:41:36 +0500

    new changes

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -6,7 +6,8 @@ class Products with ChangeNotifier {
     Product(
       id: 'p1',
       title: 'Macbook Pro',
-      description: 'Macbook Pro 2026',
+      description:
+          """MacBook Pro Apple kompaniyasining professional darajadagi noutbuklar seriyasi bo'lib, asosan kreativ mutaxassislar (dizaynerlar, videomontajchilar, dasturchilar, musiqachilar), olimlar va kuchli hisoblash quvvatiga ehtiyoj duygan foydalanuvchilar uchun mo'ljallangan.MacBook Pro - bu eng yaxshi ishlash, ajoyib ekran va uzoq batareya muddatini talab qiladigan professional foydalanuvchilar uchun eng yaxshi tanlovdir. Agar siz oddiy ofis dasturlari, internet va filmlar uchun noutbuk qidirsangiz, MacBook Air yoki boshqa brendlarning noutbuklari yanada maqbul variant bo'lishi mumkin""",
       price: 3.99,
       imageUrl:
           'https://images.unsplash.com/photo-1592919933511-ea9d487c85e4?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
@@ -14,7 +15,10 @@ class Products with ChangeNotifier {
     Product(
       id: 'p2',
       title: 'Airpods',
-      description: 'Apple Airpods',
+      description:
+          """AirPods - Apple kompaniyasining simsiz quloqchinlari boʻlib, iPhone, iPad va Mac
+       bilan bevosita va beqiyos integratsiyasi bilan mashhur. AirPods (2-avlod va 3-avlod),Standart model: Eng oddiy va kop tarqalgan versiya.Dizayn: Klassik "oqsimon" shakl. 3-avlod AirPods Pro ga oxshash qisqaroq, ammo silikon uchlari yoʻq.Xususiyatlar: Avtomatik ulanish, quloqdan olganda pauza, ikki marta bosish orqali boshqarish. 3-avlodda Fazali 
+       Stereo va maksimum tebranishli siqish (EQ) mavjud.Batareya: Taxminan 5 soat tinglash, zaryadlash qutisi bilan umumiy 24 soat.""",
       price: 2.49,
       imageUrl:
           'https://images.unsplash.com/photo-1603351154351-5e2d0600bb77?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
@@ -22,7 +26,8 @@ class Products with ChangeNotifier {
     Product(
       id: 'p3',
       title: 'Iphone 17 Pro',
-      description: 'Iphone 17 Pro',
+      description:
+          """Chiqish sanasi: Taxminan 2025-yil sentyabr oyida Apple tadbirida e'lon qilinishi kutilmoqda.2. Dizayn va ekran: Material: Titandan yasalgan ramka (iPhone 15 Pro seriyasiga o'xshash).O'lcham: 6,1 dyuym va 6,7 dyuymli Pro modellari.Ekran texnologiyasi: ProMotion teknologiyali Super Retina XDR OLED displey.Yangi imkoniyat: Har doim yoqilgan displey (Always-On Display) yangilanishlari.""",
       price: 17000,
       imageUrl:
           'https://images.unsplash.com/photo-1759588073186-1d4ac7e33623?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:food_menu/providers/cart.dart';
+import 'package:food_menu/screens/cart_screen.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/products.dart';
@@ -10,16 +12,107 @@ class ProductDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final productId = ModalRoute.of(context)!.settings.arguments as String;
+    final productId = ModalRoute.of(context)!.settings.arguments;
     final product = Provider.of<Products>(
       context,
       listen: false,
-    ).findById(productId);
+    ).findById(productId as String);
 
-    final products = Provider.of<Products>(context).list;
-
-    print(products);
-
-    return Scaffold(appBar: AppBar(title: Text(product.title)));
+    return Scaffold(
+      appBar: AppBar(title: Text(product.title)),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              height: 300,
+              child: Image.network(product.imageUrl, fit: .cover),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(product.description),
+            ),
+          ],
+        ),
+      ),
+      bottomSheet: BottomAppBar(
+        child: Row(
+          mainAxisAlignment: .spaceBetween,
+          children: [
+            Column(
+              mainAxisAlignment: .center,
+              crossAxisAlignment: .start,
+              children: [
+                const Text(
+                  "Narxi:",
+                  style: TextStyle(fontSize: 16, color: Colors.black54),
+                ),
+                Text(
+                  "\$${product.price}",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            Consumer<Cart>(
+              builder: (ctx, cart, child) {
+                final isProductAdded = cart.items.containsKey(productId);
+                if (isProductAdded) {
+                  return ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 25,
+                        vertical: 12,
+                      ),
+                      backgroundColor: Colors.grey.shade300,
+                      shape: RoundedSuperellipseBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      elevation: 0,
+                    ),
+                    onPressed: () =>
+                        Navigator.pushNamed(context, CartScreen.routeName),
+                    icon: Icon(
+                      Icons.shopping_bag_outlined,
+                      color: Colors.black,
+                    ),
+                    label: const Text(
+                      "Savatchaga borish",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  );
+                } else {
+                  return ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 25,
+                        vertical: 12,
+                      ),
+                      backgroundColor: Colors.black,
+                      shape: RoundedSuperellipseBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    onPressed: () => cart.addToCart(
+                      productId,
+                      product.title,
+                      product.imageUrl,
+                      product.price,
+                    ),
+                    child: const Text(
+                      "Savatchaga qo'shish",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  );
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
