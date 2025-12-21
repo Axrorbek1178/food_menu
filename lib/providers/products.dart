458acbc99e939564eba8f4a8b5cd8fdf724554dd
Axrorbek <axrorbekaminjonov123@gmail.com>
2025-12-21 21:46:44 +0500

    added delete function

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -111,19 +111,43 @@ class Products with ChangeNotifier {
     }
   }
 
-  void updateProduct(Product updatedProduct) {
+  Future<void> updateProduct(Product updatedProduct) async {
     final productIndex = _list.indexWhere(
       (product) => product.id == updatedProduct.id,
     );
     if (productIndex >= 0) {
-      _list[productIndex] = updatedProduct;
-      notifyListeners();
+      final url = Uri.parse(
+        "https://fir-app-7c5b7-default-rtdb.firebaseio.com/products/${updatedProduct.id}.json",
+      );
+      try {
+        await http.patch(
+          url,
+          body: jsonEncode({
+            'title': updatedProduct.title,
+            'description': updatedProduct.description,
+            'price': updatedProduct.price,
+            'imageUrl': updatedProduct.imageUrl,
+          }),
+        );
+        _list[productIndex] = updatedProduct;
+        notifyListeners();
+      } catch (e) {
+        rethrow;
+      }
     }
   }
 
-  void deleteProduct(String id) {
-    _list.removeWhere((product) => product.id == id);
-    notifyListeners();
+  Future<void> deleteProduct(String id) async {
+    final url = Uri.parse(
+      "https://fir-app-7c5b7-default-rtdb.firebaseio.com/products/$id.json",
+    );
+    try {
+      await http.delete(url);
+      _list.removeWhere((product) => product.id == id);
+      notifyListeners();
+    } catch (e) {
+      rethrow;
+    }
   }
 
   Product findById(String productId) {
